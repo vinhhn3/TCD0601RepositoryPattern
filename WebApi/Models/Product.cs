f7e18631e0ff66089eaf87af7239706c0f1e2839
Vinh Hoang <VinhHN3@fe.edu.vn>
2023-05-31 13:33:00 +0700

    Add project files.

---
+++ WebApi/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int Price { get; set; }
+    }
+}
